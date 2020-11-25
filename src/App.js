1ff90e521fc6468288074f9888f30274ec957aa1
linamitry <lina.mitry96@gmail.com>
2020-11-25 17:22:25 +0300

    css

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+
+
+const App = ()=> {
+  return (
+    <div className="app-wrapper"> 
+      <header className="header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZW4Q3Zs1NXm8CaDPRuhOQ7GjIBuw7qtTXfQ&usqp=CAU" />
+      </header>
+      <nav className="nav">
+        <div>
+          Profile
+        </div>
+        <div>
+          Massages
+        </div>
+      </nav>
+      <div className="content">
+        Main Content
+      </div>      
+    </div>
+)
+}
+export default App;
+ 
